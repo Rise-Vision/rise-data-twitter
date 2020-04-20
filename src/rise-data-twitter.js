176118ac2448d2c76765c66f2e2d56e942516f0f
Stuart Lees <stuartlees@gmail.com>
2020-04-20 16:50:04 -0400

    Refactor service request url to adhere to latest get-presentation-tweets endpoint

--- src/rise-data-twitter.js
+++ src/rise-data-twitter.js
@@ -121,18 +121,17 @@ export default class RiseDataTwitter extends FetchMixin(fetchBase) {
   }
 
   _getUrl() {
-    const companyId = RisePlayerConfiguration.getCompanyId();
-    const username = this.username && this.username.indexOf("@") === 0 ?
-      this.username.substring(1) : this.username;
+    const presentationId = RisePlayerConfiguration.getPresentationId(),
+      hash = this._computeHash( presentationId, this.id, this.username );
 
     return `${
       config.twitterServiceURL
-    }/get-tweets?companyId=${
-      companyId
-    }&username=${
-      username
-    }&count=${
-      this.maxitems
+    }/get-presentation-tweets?presentationId=${
+      presentationId
+    }&componentId=${
+      this.id
+    }&hash=${
+      hash
     }`;
   }
 
